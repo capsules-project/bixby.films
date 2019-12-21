9cf66a558caa9d9a2a3a3d6c6dabee47ee414792
cmmario94 <cmmario94@gmail.com>
2019-12-22 00:49:22 +0100

    minor bug in image scrapping fixed and deprecated warnings

--- code/lib/HTMLParser.js
+++ code/lib/HTMLParser.js
@@ -3,7 +3,8 @@ var http = require('http') ;
 
 function htmlParser(id) {
   var html = http.getUrl('https://www.filmaffinity.com/es/film' + id + '.html') ;
-  var image = html.split("movie-main-image-container")[1].split('<img itemprop')[1].split('src="')[1].split('"alt')[0]
+  console.log(html)
+  var image = html.split("movie-main-image-container")[1].split('itemprop')[1].split('src="')[1].split('" alt=')[0]
   // console.log(image)
   return image
 }
