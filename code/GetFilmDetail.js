6c67ba82879ab09902b7f08c838000e13923a09a
cmmario94 <cmmario94@gmail.com>
2019-12-30 14:17:17 +0100

    API change to TheMovieDB API

--- code/GetFilmDetail.js
+++ code/GetFilmDetail.js
@@ -5,11 +5,11 @@ var _filmDetail = require('./lib/FilmDetail');
 var console = require('console') ;
 
 function getFilmDetail(film, $vivContext) {
-  console.log('getFilmDetail, id: ',film.id) ;
+  // console.log('getFilmDetail, id: ',film.id) ;
   var api = new _api.API();
   var filmInfo = api.getFilmDetails(film.id);
-  var parser = new _filmDetail.FilmDetail(film.id, filmInfo, $vivContext) ;
-  console.log('getFilmDetail, parsedFilm: ', parser)
+  var parser = new _filmDetail.FilmDetail(filmInfo, $vivContext) ;
+  // console.log('getFilmDetail, parsedFilm: ', parser)
   return parser
 } ;
 
--- code/lib/API.js
+++ code/lib/API.js
@@ -3,33 +3,74 @@ var http = require('http') ;
 var console = require('console') ;
 
 function API() {
-  this.getFilms = function getFilms(searchTerm) {
-    var searchTerms = searchTerm[0] ;
-    if (searchTerm.length > 1) {
-      for (var i=1 ; i < searchTerm.length ; i++) {
-        var searchTermList = searchTerms + searchTerm[i] ;
-        var searchTermList = searchTermList.split(' ') ;
-      }
-    }
-    else {
-      var searchTermList = searchTerms.split(' ') ;
-    }
-    var query = encodeURIComponent(searchTermList[0]) ;
-    for (var i=1 ; i < searchTermList.length ; i++) {
-      query = query + '+' + encodeURIComponent(searchTermList[i])
-    } ;
-    // console.log('query: ', query) ;
-    var api = http.getUrl(config.get('apiSearch') + '/' + query) ;
+  this.getConfig = function getConfig() {
+    var query = http.makeQueryString({
+      "api_key": "fd54ccf1ef3a6b27f6f0f62a20a5cc96"
+    });
+    var api = http.getUrl(config.get('apiConfig') + '?' + query)
+    // console.log(api)
     api = JSON.parse(api);
     return api;
   };
 
-  this.getFilmDetails = function getFilmDetails(id) {
-      var api = http.getUrl(config.get('apiFilm') + '/' + id);
-      api = JSON.parse(api);
-      return api;
+  this.getFilms = function getFilms(searchTerm) {
+    var query = http.makeQueryString({
+      "api_key": "fd54ccf1ef3a6b27f6f0f62a20a5cc96",
+      "query": searchTerm,
+      "language": "es-ES"
+    });
+    // console.log('query: ', query) ;
+    var api = http.getUrl(config.get('apiSearch') + '?' + query) ;
+    api = JSON.parse(api);
+    return api['results']
   };
+
+  this.getFilmDetails = function getFilmDetails(id) {
+    var query = http.makeQueryString({
+      "api_key": "fd54ccf1ef3a6b27f6f0f62a20a5cc96",
+      "language": "es-ES"
+    });
+    var api = http.getUrl(config.get('apiFilm') + '/' + id + '?' + query);
+    api = JSON.parse(api);
+    return api;
+  };
+
+  this.getFeaturedPeople = function getFeaturedPeople(id) {
+    var ftPeople = {}
+    var query = http.makeQueryString({
+      "api_key": "fd54ccf1ef3a6b27f6f0f62a20a5cc96",
+    });
+    var api = http.getUrl(config.get('apiFilm') + '/' + id + '/credits' + '?' + query);
+    api = JSON.parse(api);
+  
+    // getting people
+    for (var i=0; i < api['crew'].length; i++) {
+      if (api['crew'][i]['job'] == 'Director') {
+        ftPeople.director = api['crew'][i]['name']
+        break
+      }
+    };
+
+    ftPeople.ftCast = []
+    for (var i=0; i < api['cast'].length; i++)  {
+      if (api['cast'][i]['order'] <= 10) {
+        ftPeople.ftCast.push(api['cast'][i]['name'])
+      } else {
+        break
+      }
+    }
+    return ftPeople
+  };
+
+  this.getIMDbID = function getIMDbID(id) {
+    var query = http.makeQueryString({
+      "api_key": "fd54ccf1ef3a6b27f6f0f62a20a5cc96",
+    });
+    var api = http.getUrl(config.get('apiFilm') + '/' + id + '/external_ids' + '?' + query);
+    api = JSON.parse(api);
+    return api['imdb_id'];
   }
+  };
 
 module.exports = {
   API: API
